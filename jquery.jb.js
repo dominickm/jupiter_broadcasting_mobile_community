629fd4aa34c3b6f5908af44fb7edb70a34324697
Daniel Samson <danieljsamson@gmail.com>
2012-07-28 19:58:41 +0100

    Create jquery.jb.js
    Create the file that will contain all the jQuery based logic for the
    application. Since it will be using jQuery I have named it using the same
    convention. I have implemented the jb namespace and the jb.player
    namespace to prevent future naming conflicts.
    
    The jb.player still needs to be implemented.

---
+++ jquery.jb.js
@@ -0,0 +1,38 @@
+/*
+Copyright [2012] [Fingertip Tech, INC]
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+-->
+*/
+// use JB Namespace to prevent naming conflicts
+var jb = {
+	player : {
+		"isPlaying": false,
+		"url" : null,
+		"play" : function () {
+			"use strict";
+		},
+		"stop" : function () {
+			"use strict";
+		},
+		"pause" : function () {
+			"use strict";
+		}
+	},
+	url : {
+		"get" : function () {
+			"use strict";
+		}
+	}
+};
+//jb.player.play();
