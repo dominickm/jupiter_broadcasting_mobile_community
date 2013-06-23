1a472d7022350f87ee1d4594fa1971f69681d1e9
Godzilla <jason@theograys.com>
2013-06-22 20:35:22 -0400

    Initial commit with unit tests, as well as with basic DOM testing. This closes #10

---
+++ tests/spec/JbSpec.js
@@ -0,0 +1,24 @@
+describe("JB", function(){
+  it("Should load the feed upon first launch", function(){
+    var theNumberOfChildren = $('#body').children().length
+    expect(theNumberOfChildren).toBe(1);
+  });
+
+  it("Should set the header upon first launch", function() {
+    var applicationName = $('#header > center > h1').text()
+    expect(applicationName).toBe(jb._APP_NAME);
+  });
+
+  describe("The rss feed.", function(){
+    it("Should not return an error", function() {
+      jb.feed.get(jb.feed._OGG_FEED, function(results){
+        expect(results.status.code).toBe(200);
+      });
+    });
+    it("Should contain 10 results.", function(){
+      jb.feed.get(jb.feed._OGG_FEED, function(results){
+        expect(results.feed.entries.length).toBe(10);
+      });
+    });
+  });
+});
